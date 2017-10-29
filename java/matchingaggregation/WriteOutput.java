5b9d269609195f0e4cd67c62958f107928708ded
MSchalken <M.M.R.Schalken@student.tue.nl>
2017-10-29 13:42:52 +0100

    now with all changes

--- java/matchingaggregation/WriteOutput.java
+++ java/matchingaggregation/WriteOutput.java
@@ -1,4 +1,41 @@
 package matchingaggregation;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Created by MSchalken on 29/10/2017.
+ */
 public class WriteOutput {
+
+    public static void writeJSON(File file, Grid g) throws IOException, JSONException {
+        JSONObject json = toJSON(g);
+
+        try (FileWriter writer = new FileWriter(file)) {
+            writer.append(json.toString());
+        }
+
+    }
+
+    private static JSONObject toJSON(Grid g) throws JSONException {
+        JSONObject json = new JSONObject();
+        json.put("width", g.getWidth());
+        json.put("height", g.getHeight());
+
+        JSONArray points = new JSONArray();
+        for (int x = 0; x < g.getWidth(); x++) {
+            for (int y = 0; y < g.getHeight(); y++) {
+                if (g.getXY(x,y) > 0) {
+                    points.put(new JSONArray("[" + x + "," + "y" + "," + g.getXY(x,y) + "]"));
+                }
+            }
+        }
+        json.put("points", points);
+        return json;
+    }
+
 }
